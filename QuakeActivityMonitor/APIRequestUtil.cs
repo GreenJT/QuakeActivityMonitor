14655a6be46303c5d0530a1b9f19d226c7fcfdf3
Joshua <greenjoshuat@gmail.com>
2016-07-28 17:19:45 -0700

    - Made APIRequestUtil.cs to serve as a utility to consume the data from an APIRequest
    - Added config info to app.config
    - Added code to Program.cs to output earthquakes within the last hour

---
+++ QuakeActivityMonitor/APIRequestUtil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace QuakeActivityMonitor
+{
+    public class APIRequestUtil
+    {
+        private String BaseUrl;
+        private String Parameters;
+        private RootObject JsonOutput;
+
+        public APIRequestUtil(String baseUrl, String parameters)
+        {
+            this.BaseUrl = baseUrl;
+            this.Parameters = parameters;
+        }
+
+        public RootObject ConsumeAPI()
+        {
+            RunAsync().Wait();
+            return JsonOutput;
+        }
+
+        private async Task RunAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(Parameters);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonOutput = response.Content.ReadAsStringAsync().Result;
+                    JsonOutput = JsonConvert.DeserializeObject<RootObject>(jsonOutput);
+                }
+
+            }
+        }
+    }
+}
--- QuakeActivityMonitor/Program.cs
+++ QuakeActivityMonitor/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace QuakeActivityMonitor
 {
@@ -10,14 +7,45 @@ namespace QuakeActivityMonitor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press ESC to stop");
-            do
-            {
-                while (!Console.KeyAvailable)
-                {
+            DateTime dateTime = DateTime.UtcNow.AddHours(-1);
+                        
+            var baseAddress = ConfigurationManager.AppSettings["BaseAddress"].ToString();
+            var method = ConfigurationManager.AppSettings["Method"].ToString();
+            var format = ConfigurationManager.AppSettings["Format"].ToString();
+            var firstParameter = ConfigurationManager.AppSettings["Parameter"].ToString();
+            var orderby = ConfigurationManager.AppSettings["OrderBy"].ToString();
 
-                }
-            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            firstParameter = firstParameter + dateTime.ToString("s");
+
+            var parameter = method + format + firstParameter + orderby;
+
+            APIRequestUtil request = new APIRequestUtil(baseAddress, parameter);
+            var pastHourActivity = request.ConsumeAPI();
+            
+            for(int i = 0; i < pastHourActivity.Metadata.Count; i++)
+            {
+                var properties = pastHourActivity.Features[i].Properties;
+                var geometry = pastHourActivity.Features[i].Geometry;
+                var time = new DateTime(1970, 1, 1, 0, 0, 0).AddMilliseconds(Convert.ToInt64(properties.Time)).ToString();
+                var magnitude = properties.Mag;
+                var coordinates = geometry.Coordinates[0] + ", " + geometry.Coordinates[1];
+
+                var output = $@"Date/Time: {time}{Environment.NewLine}Magnitude: {magnitude}{Environment.NewLine}Coordinates: {coordinates}";
+
+                Console.WriteLine(output);
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+
+            //Console.WriteLine("Press ESC to stop");
+            //do
+            //{
+            //    while (!Console.KeyAvailable)
+            //    {
+
+            //    }
+            //} while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
